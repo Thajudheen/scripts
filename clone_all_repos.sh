04b68f62d78b5914b7153ae26b24290e6888c778
Thaj <hellothaj@gmail.com>
2022-08-06 19:39:36 +0200

    Remove token

--- clone_all_repos.sh
+++ clone_all_repos.sh
@@ -4,7 +4,7 @@ GH_API_TOKEN=''
 # GitHub User Name
 GH_USER='abcd'
 # 1st workiking CLI to clone repos thro API call
-curl -X GET -u thajudheen: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 https://api.github.com/users/thajudheen/repos | grep -e 'clone_url*' |\
+curl -X GET -u thajudheen:aaaaa https://api.github.com/users/thajudheen/repos | grep -e 'clone_url*' |\
   cut -d \" -f 4 |\
   xargs -L1 git clone
 
@@ -18,9 +18,10 @@ curl -H 'Authorization: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' "https://api.gi
 
 curl -H 'Authorization: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' "https://api.github.com/users/$GHUSER/repos?&per_page=1000" | jq -r '.[]|.clone_url' | xargs -L1 git clone
 
-#script
+# Call the function in Bash script
 
 # Usage: gh-clone-user (user)
 gh-clone-user() {
   curl -sL "https://api.github.com/users/$1/repos?per_page=1000" | jq -r '.[]|.clone_url' | xargs -L1 git clone
+}
 
